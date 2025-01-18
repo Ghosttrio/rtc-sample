443ccee8ddc505335a8b9f51de93ec3a89ce860b
Ghosttrio <ghosttrio@naver.com>
2025-01-18 12:47:39 +0900

    .

---
+++ src/main/kotlin/com/ghosttrio/rtcsample/RtcSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.ghosttrio.rtcsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RtcSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<RtcSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/ghosttrio/rtcsample/RtcSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ghosttrio.rtcsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RtcSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
